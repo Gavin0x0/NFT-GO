0673e51b072803d69873ab1d8052b3b9af8d2db0
Levi_o <632891553@qq.com>
2021-05-14 21:34:04 +0800

    框架搭建完成

---
+++ src/views/Frame.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
